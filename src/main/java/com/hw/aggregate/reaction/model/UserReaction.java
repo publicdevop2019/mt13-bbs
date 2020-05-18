57626ad1c2f04fce9c66b7b3136c692e68ebbc4d
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-05-18 15:39:23 -0400

    refactor code

--- src/main/java/com/hw/aggregate/reaction/model/UserReaction.java
+++ src/main/java/com/hw/aggregate/reaction/model/UserReaction.java
@@ -9,10 +9,6 @@ import com.hw.shared.Auditable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
-import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.*;
 import java.util.Map;
@@ -41,9 +37,7 @@ public class UserReaction extends Auditable {
                                       ReactionEnum reactionEnum,
                                       Map<ReferenceEnum, ReferenceService> refServices,
                                       ReactionRepository reactionRepository,
-                                      String userId,
-                                      PlatformTransactionManager transactionManager,
-                                      EntityManager entityManager
+                                      String userId
     ) {
         if (refId == null || refEnum == null || reactionEnum == null || refServices == null || reactionRepository == null || userId == null)
             throw new FieldValidationException();
@@ -55,16 +49,18 @@ public class UserReaction extends Auditable {
         if (reactionEnum.equals(ReactionEnum.LIKE)) {
             Optional<UserReaction> reaction = reactionRepository.findReaction(userId, refId, refEnum, ReactionEnum.DISLIKE);
             if (reaction.isPresent()) {
-                log.info("adding like, remove dislike if exist");
-                return update(reaction.get(), new UserReaction(refId, refEnum, reactionEnum), transactionManager, entityManager, reactionRepository);
+                log.info("update like to dislike if exist");
+                reaction.get().setReactionType(ReactionEnum.DISLIKE);
+                return reactionRepository.save(reaction.get());
             } else {
                 return reactionRepository.save(new UserReaction(refId, refEnum, reactionEnum));
             }
         } else if (reactionEnum.equals(ReactionEnum.DISLIKE)) {
             Optional<UserReaction> reaction = reactionRepository.findReaction(userId, refId, refEnum, ReactionEnum.LIKE);
             if (reaction.isPresent()) {
-                log.info("adding dislike, remove like if exist");
-                return update(reaction.get(), new UserReaction(refId, refEnum, reactionEnum), transactionManager, entityManager, reactionRepository);
+                log.info("update dislike to like if exist");
+                reaction.get().setReactionType(ReactionEnum.LIKE);
+                return reactionRepository.save(reaction.get());
             } else {
                 return reactionRepository.save(new UserReaction(refId, refEnum, reactionEnum));
             }
@@ -74,59 +70,10 @@ public class UserReaction extends Auditable {
 
     }
 
-    public static UserReaction create(String refId,
-                                      ReferenceEnum refEnum,
-                                      ReactionEnum reactionEnum,
-                                      Map<ReferenceEnum, ReferenceService> refServices,
-                                      ReactionRepository reactionRepository,
-                                      String userId) {
-        if (refId == null || refEnum == null || reactionEnum == null || refServices == null || reactionRepository == null || userId == null)
-            throw new FieldValidationException();
-        ReferenceService refService = refServices.get(refEnum);
-        if (refService == null)
-            throw new ReferenceServiceNotFoundException();
-        if (!refService.existById(refId))
-            throw new ReferenceNotFoundException();
-        if (reactionEnum.equals(ReactionEnum.LIKE))
-            try {
-                delete(userId, refId, refEnum, ReactionEnum.DISLIKE, reactionRepository);
-            } catch (ReactionNotFoundException ex) {
-                //ignore on purpose
-            }
-        if (reactionEnum.equals(ReactionEnum.DISLIKE))
-            try {
-                delete(userId, refId, refEnum, ReactionEnum.LIKE, reactionRepository);
-            } catch (ReactionNotFoundException ex) {
-                //ignore on purpose
-            }
-        return reactionRepository.save(new UserReaction(refId, refEnum, reactionEnum));
-    }
-
     private UserReaction(String referenceId, ReferenceEnum referenceType, ReactionEnum reactionEnum) {
         this.referenceId = referenceId;
         this.referenceType = referenceType;
         this.reactionType = reactionEnum;
-    }
-
-    /**
-     * hibernate fixed delete-insert order fix
-     *
-     * @return
-     */
-    private static UserReaction update(UserReaction toBeUpdated, UserReaction toBeInserted,
-                                       PlatformTransactionManager transactionManager, EntityManager entityManager,
-                                       ReactionRepository reactionRepository
-    ) {
-        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-            @Override
-            protected void doInTransactionWithoutResult(TransactionStatus status) {
-                toBeUpdated.setReactionType(toBeInserted.getReactionType());
-                entityManager.persist(toBeUpdated);
-                entityManager.flush();
-            }
-        });
-        return toBeInserted;
     }
 
     public static void delete(String userId, String refId, ReferenceEnum referenceEnum, ReactionEnum reactionEnum, ReactionRepository reactionRepository) {
